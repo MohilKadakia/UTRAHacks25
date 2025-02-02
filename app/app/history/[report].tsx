1f3230e58dc5c2acdc0d4814a7a57e7d2aeab252
MohilKadakia <mohilk706@proton.me>
2025-02-01 22:15:08 -0500

    History of workouts page

--- app/app/history/[report].tsx
+++ app/app/history/[report].tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, TextInput } from 'react-native';
+import { Image, StyleSheet, Platform, PermissionsAndroid, TextInput } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -9,37 +9,11 @@ import { HapticTab } from '@/components/HapticTab';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { router } from 'expo-router';
+import Graph from '@/components/Graph';
 
 export default function HomeScreen() {
 
   const [number, setNumber] = useState('');
-
-
-  // Store IP Address so it doesn't have to be typed each time
-  const storeData = async (value : string) => {
-    try {
-      await AsyncStorage.setItem('ipaddr', value);
-      router.push('/history')
-    } catch (e) {
-      return false;
-    }
-  };
-
-  // Get IP Address from storage if exists
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const value = await AsyncStorage.getItem('ipaddr');
-        if (value !== null) {
-          setNumber(value);
-        }
-      } catch (e) {
-        // error reading value
-      }
-    };
-
-    getData();
-  }, [])
 
   return (
     <ParallaxScrollView
@@ -50,18 +24,14 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
+
+      {/* <Graph></Graph> */}
+      
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">TEST PAGE</ThemedText>
       </ThemedView>
-      <TextInput
-        style={styles.input}
-        onChangeText={setNumber}
-        value={number}
-        placeholder="IP Address"
-        keyboardType="numeric"
-      />
       <HapticTab 
-        onPress={() => {storeData(number)}}
+        onPress={() => {}}
         style={styles.confirmButton}
         ><ThemedText>Confirm: {number}</ThemedText></HapticTab>
     </ParallaxScrollView>
--- app/components/ExerciseHistoryCard.tsx
+++ app/components/ExerciseHistoryCard.tsx
@@ -5,18 +5,20 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { HapticTab } from './HapticTab';
+import { router } from 'expo-router';
 
 type Props = PropsWithChildren<{
   title: string;
   image: ReactElement;
+  id: string;
 }>;
 
 export function ExerciseCard({
   children,
   title,
   image,
+  id,
 }: Props) {
-  const [isOpen, setIsOpen] = useState(false);
   const theme = useColorScheme() ?? 'light';
 
   const cardColor = {
@@ -28,17 +30,13 @@ export function ExerciseCard({
     <ThemedView style={[styles.card, {backgroundColor: cardColor[theme]}]}>
       <TouchableOpacity
         style={styles.heading}
-        onPress={() => setIsOpen((value) => !value)}
+        onPress={() => {router.push(`/history/${id}`)}}
         activeOpacity={0.8}>
         {image}
         
         <ThemedText type="title">{title}</ThemedText>
       </TouchableOpacity>
       
-      {/* Start Exercise Session Button */}
-      {isOpen && <ThemedView style={styles.content}> 
-        <HapticTab style={styles.startButton}>Start</HapticTab>
-      </ThemedView>}
     </ThemedView>
   );
 }
@@ -56,13 +54,5 @@ const styles = StyleSheet.create({
   card: {
     padding: 24,
     borderRadius: 12,
-  },
-  startButton: {
-    marginTop: 12,
-    paddingVertical: 10,
-    alignItems: 'center',
-    backgroundColor: '#457f35',
-    borderRadius: 12,
-    maxWidth: 96,
   },
 });
